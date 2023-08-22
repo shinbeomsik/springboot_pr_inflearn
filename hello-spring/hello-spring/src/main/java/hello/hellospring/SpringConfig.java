28c099927f35b96dd68517faa551c249e243ca1f
shinbeomsik <sbsdkssud@gmail.com>
2023-08-22 20:44:36 +0900

    AOP 적용

--- hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.JpaMemberRepository;
@@ -43,6 +44,10 @@ public class SpringConfig {
 		public MemberService memberService() {
 			return new MemberService(memberRepository);
 		}
+		
+		/*
+		 * @Bean public TimeTraceAop timeTraceAop() { return new TimeTraceAop(); }
+		 */
 		
 		//@Bean
 		//public MemberRepository memberRepository() {
--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -26,19 +26,12 @@ public class MemberService {
 	 */
 	public Long join(Member member) {
 		
-		long start = System.currentTimeMillis();
 		
-		try {
-			  
 			validateDuplicateMember(member); //중복 회원 검증
 			
 			memberRepository.save(member);
 			return member.getId();
-		} finally {
-			long finish = System.currentTimeMillis();
-			long timeMs = finish - start;
-			System.out.println("join = " + timeMs + "ms");
-		}
+		
 		// 같은 이름이 있는 중복 회원 x
 	}
 
@@ -52,16 +45,7 @@ public class MemberService {
 	 * 전체 회원 조회 
 	 */
 	public List<Member> findMembers(){
-		
-		long start = System.currentTimeMillis();
-		try {
-			
 			return memberRepository.findAll();
-		} finally {
-			long finish = System.currentTimeMillis();
-			long timeMs = finish - start;
-			System.out.println("findMembers =" + timeMs + "ms");
-		}
 		
 	}
 	
